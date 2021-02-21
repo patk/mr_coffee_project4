c50ce49e2eae8a14b68961d637a79a25a530b3d2
Pat <pat.ksarach@gmail.com>
2021-02-21 14:36:42 +1100

    Set up Express, EJS and Morgan 2.0

---
+++ database.js
@@ -0,0 +1,9 @@
+// database configurations
+
+// create database connection between express application and postgres database
+const pgp = require("pg-promise")(); // require the package and immediately call the package
+const connection = "postgres://postgres:1102448095@localhost:5432/mr_coffee";
+const db = pgp(connection);
+
+// export to let other files access the database
+module.exports = db;
--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // set up express
 const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set up morgan
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+// set up database
+const database = require("./database");
+
+// set up view engine
+app.set("view engine", "ejs");
+
+// port
+const PORT = 3200;
+app.listen(PORT, () => {
+  console.log("server is listening on localhost " + PORT);
+});
+
+// create routes here
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
