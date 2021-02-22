77d599d87cbd71c5295c34359acf7372011036cd
Pat <pat.ksarach@gmail.com>
2021-02-22 17:07:18 +1100

    Seed schedules table with dummy data

---
+++ sql/seed_schedules_table.sql
@@ -0,0 +1,17 @@
+INSERT INTO schedules(user_id, day, start_time, end_time)
+VALUES (1, 3, '2021-03-24 10:30:00', '2021-03-24 12:00:00');
+
+INSERT INTO schedules(user_id, day, start_time, end_time)
+VALUES (1, 5, '2021-03-26 16:20:00', '2021-03-26 18:30:00');
+
+INSERT INTO schedules(user_id, day, start_time, end_time)
+VALUES (2, 1, '2021-02-22 10:00:00', '2021-02-22 13:50:00');
+
+INSERT INTO schedules(user_id, day, start_time, end_time)
+VALUES (2, 4, '2021-03-04 13:00:00', '2021-03-04 14:30:00');
+
+INSERT INTO schedules(user_id, day, start_time, end_time)
+VALUES (3, 6, '2021-03-20 08:00:00', '2021-03-20 11:30:00');
+
+INSERT INTO schedules(user_id, day, start_time, end_time)
+VALUES (3, 7, '2021-03-28 21:00:00', '2021-03-28 23:30:00');
